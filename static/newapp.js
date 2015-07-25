4a3b076f09c224ef94735772d905363f4c548c9c
jesicamarquez <jesica.v.marquez@gmail.com>
2015-07-25 14:48:34 -0700

    added form

--- static/newapp.js
+++ static/newapp.js
@@ -16,17 +16,19 @@ $("body").append($overlay);
 
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
-  event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  //Update overlay with the image linked in the link
-  $image.attr("src", imageLocation);
-  
-  //Show the overlay.
-  $overlay.show();
-  
-  //Get child's alt attribute and set caption
-  var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
+  if (this != document.getElementByID("li-redirect")){
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
+    //Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+    
+    //Show the overlay.
+    $overlay.show();
+    
+    //Get child's alt attribute and set caption
+    var captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText);
+  }
 });
 
 //When overlay is clicked
